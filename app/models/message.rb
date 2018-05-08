497596ef20f9abebcaa8bda1c063a12d58d008ae
takehir00 <0kutakehiro15@gmail.com>
2018-05-08 14:44:13 +0900

    add_carrier_wave

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,5 @@
 class Message < ApplicationRecord
   belongs_to :user
   belongs_to :group
+  mount_uploader :avatar, AvatarUploader
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507102726) do
+ActiveRecord::Schema.define(version: 20180508052623) do
 
   create_table "group_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "group_id"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20180507102726) do
     t.integer  "group_id",                 null: false
     t.datetime "created_at",               null: false
     t.datetime "updated_at",               null: false
+    t.string   "avatar"
     t.index ["group_id"], name: "index_messages_on_group_id", using: :btree
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
   end
