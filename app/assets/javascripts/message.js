59d110f37f262e7e7d442ab7decb9dfe2b84a2d6
takehir00 <0kutakehiro15@gmail.com>
2018-05-14 20:36:07 +0900

    message-ajax2

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,26 @@
 $(function() {
   function buildHTML(message){
-    var html = `<h2 class = chat__content1__name>
+    var html = `<h2 class = "chat__content1__name">
                     ${message.user_name}</h2>
-                <p class = chat__content1__date>
-                ${message.created_at}</p>`
+                <p class = "chat__content1__date">
+                    ${message.created_at}</p>
+                <div class = "chat__content1__message">
+                      ${message.body}</div>
+                <div>
+                  <img src= ${message.avatar_url}></div>`
+    return html;
   }
+  function buildHTML_no_image(message){
+    var html = `<h2 class = "chat__content1__name">
+                    ${message.user_name}</h2>
+                <p class = "chat__content1__date">
+                    ${message.created_at}</p>
+                <div class = "chat__content1__message">
+                      ${message.body}</div>
+                `
+    return html;
+  }
+
   $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -17,9 +33,23 @@ $(function() {
         processData: false,
         contentType: false
       })
+
       .done(function(data){
-        var html = buildHTML(data);
-        $('.chat_content').append(html)
+        console.log(data);
+        if (data['avatar_url'] != null){
+          var html = buildHTML(data);
+        } else {
+          var html = buildHTML_no_image(data);
+        }
+
+
+        $('.chat__content').append(html)
+        var target = $(".chat__content").get(0).scrollHeight;
+        $('.chat__content').animate({scrollTop: target}, 'slow');
       })
+
+      .fail(function(){
+      alert('error');
+    })
   });
 });
