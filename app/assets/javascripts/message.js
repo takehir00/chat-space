8266575bd44ecc46e75708f943accf5765456f2d
takehir00 <0kutakehiro15@gmail.com>
2018-05-15 18:34:25 +0900

    modify_HTML

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,26 +1,19 @@
 $(function() {
   function buildHTML(message){
-    var html = `<h2 class = "chat__content1__name">
-                    ${message.user_name}</h2>
-                <p class = "chat__content1__date">
-                    ${message.created_at}</p>
-                <div class = "chat__content1__message">
-                      ${message.body}</div>
-                <div>
-                  <img src= ${message.avatar_url}></div>`
-    return html;
-  }
-  function buildHTML_no_image(message){
-    var html = `<h2 class = "chat__content1__name">
-                    ${message.user_name}</h2>
-                <p class = "chat__content1__date">
-                    ${message.created_at}</p>
-                <div class = "chat__content1__message">
-                      ${message.body}</div>
-                `
-    return html;
-  }
+    if (message.image_url != null){
+      var image = `<img src = "${message.image_url}">`
+    }
+     var html = `<h2 class = "chat__content1__name">
+                     ${message.user_name}</h2>
+                 <p class = "chat__content1__date">
+                     ${message.created_at}</p>
+                 <div class = "chat__content1__message">
+                       ${message.body}</div>
 
+                <div>
+                  ${image}</div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
       e.preventDefault();
       var formData = new FormData(this);
@@ -35,14 +28,7 @@ $(function() {
       })
 
       .done(function(data){
-        console.log(data);
-        if (data['avatar_url'] != null){
-          var html = buildHTML(data);
-        } else {
-          var html = buildHTML_no_image(data);
-        }
-
-
+        var html = buildHTML(data)
         $('.chat__content').append(html)
         var target = $(".chat__content").get(0).scrollHeight;
         $('.chat__content').animate({scrollTop: target}, 'slow');
