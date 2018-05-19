5c925fb8dffb682807f888a0dc1dda6628b6b41b
takehir00 <0kutakehiro15@gmail.com>
2018-05-19 21:53:04 +0900

    modify_indent

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -4,19 +4,27 @@ $(document).on("turbolinks:load", function() {
 
   function appendUser(user) {
     var html =`<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.name}</p>
-                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name= ${user.name}>追加</a>
-              </div>`
+                <p class="chat-group-user__name">
+                  ${user.name}
+                </p>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name= ${user.name}>
+                  追加
+                </a>
+                </div>`
     user_list.append(html)
   }
 
   function appendUserResult(userId, userName) {
     var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
                 <input name='group[user_ids][]' type='hidden' value= '${userId}'>
-                <p class='chat-group-user__name'>'${userName}'</p>
-                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
-                </div>`
-        return html;
+                <p class='chat-group-user__name'>
+                  ${userName}
+                </p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>
+                  削除
+                </a>
+              </div>`
+    return html;
   }
 
   $("#user-search-field").on("keyup", function() {
@@ -25,25 +33,24 @@ $(document).on("turbolinks:load", function() {
     if(input.length === 0){
       ("#user-search-result").empty();
     }else{
+      $.ajax({
+        type:'GET',
+        url:'/users',
+        data: {keyword: input},
+        dataType:'json'
+      })
 
-    $.ajax({
-      type:'GET',
-      url:'/users',
-      data: {keyword: input},
-      dataType:'json'
-    })
-
-    .done(function(users){
-      $("#user-search-result").empty();
+      .done(function(users){
+        $("#user-search-result").empty();
         users.forEach(function(user){
           appendUser(user);
         });
-          })
-
-     .fail(function() {
-      alert('ユーザー検索に失敗しました');
     })
-   }
+
+      .fail(function() {
+        alert('ユーザー検索に失敗しました');
+      })
+    }
   });
 
   $("#user-search-result").on("click", ".chat-group-user__btn--add", function(){
