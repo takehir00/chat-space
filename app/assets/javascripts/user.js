e1a97285d28c2c5c589eb5264522f5a96658d440
takehir00 <0kutakehiro15@gmail.com>
2018-05-19 20:11:12 +0900

    modify_javascript

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,17 +1,31 @@
-$(function(){
+$(document).on("turbolinks:load", function() {
 
   var user_list = $("#user-search-result");
 
   function appendUser(user) {
     var html =`<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name= ${user.name}>追加</a>
+                <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id = ${user.id} data-user-name= ${user.name}>追加</a>
               </div>`
     user_list.append(html)
   }
 
+  function appendUserResult(userId, userName) {
+    var html =`<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-8'>
+                <input name='group[user_ids][]' type='hidden' value= '${userId}'>
+                <p class='chat-group-user__name'>'${userName}'</p>
+                <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+        return html;
+  }
+
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
+
+    if(input.length === 0){
+      ("#user-search-result").empty();
+    }else{
+
 
     $.ajax({
       type:'GET',
@@ -30,9 +44,19 @@ $(function(){
      .fail(function() {
       alert('ユーザー検索に失敗しました');
     })
+   }
   });
 
-  $(document).on("click", ".chat-group-user__btn--add", function(){
-    console.log(this);
+  $("#user-search-result").on("click", ".chat-group-user__btn--add", function(){
+    var userId = $(this).data('user-id');
+    var userName = $(this).data('user-name');
+    var html = appendUserResult(userId,userName);
+    $("#chat-group-users").append(html);
+    $(this).parent().remove();
+  });
+
+  $("#chat-group-users").on("click",".chat-group-user__btn--remove", function(){
+    $(this).parent().remove();
   })
+
 });
