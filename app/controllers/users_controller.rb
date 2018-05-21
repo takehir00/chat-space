a13e4360faa2281cc75b362b7f95df80e8fa5eff
takehir00 <0kutakehiro15@gmail.com>
2018-05-21 15:05:41 +0900

    not_search_currentUser

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.where("name like ?","%#{ params[:keyword] }%")
+    @users = User.where("name like ?","%#{ params[:keyword] }%").where.not(id: current_user.id)
       respond_to do |format|
         format.html { redirect_to root_path }
         format.json
