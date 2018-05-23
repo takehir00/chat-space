6c1d40bc23003980dae057b5101abecedb4f0eb3
takehir00 <0kutakehiro15@gmail.com>
2018-05-23 21:24:47 +0900

    deletre_image

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :message do
     body Faker::Lorem.sentence
-    image File.open("#{Rails.root}/public/images/river2.jpeg")
+    #image File.open("#{Rails.root}/public/images/river2.jpeg")
     user
     group
   end
