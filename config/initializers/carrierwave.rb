c08ceb0110e6d85c33dc488e14d4b385f0003007
takehir00 <0kutakehiro15@gmail.com>
2018-05-25 17:59:20 +0900

    modify_carierwave3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -12,5 +12,5 @@ CarrierWave.configure do |config|
   }
 
   config.fog_directory  = 'chat-upload-test'
-  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/ここにバケット名を入れます'
+  config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-upload-test'
 end
